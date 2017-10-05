ed8c28ea5ccf483dd011d4eb6b4df619a399fdf9
Corey O'Brien <corey@reactiveops.com>
2017-10-05 16:05:07 -0400

    Don't try to reclaim IPs from existing nodes, even if they're unreachable

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM quay.io/reactiveops/kubectl-runner:latest
 
+RUN apk --no-cache add coreutils
+
 ADD run.sh ./
 CMD ["bash", "run.sh"]
--- run.sh
+++ run.sh
@@ -12,12 +12,13 @@ do
   weave_pod=$(kubectl get pods -n kube-system | grep weave | awk -F ' ' '{print $1}' | shuf | head -1 )
   echo "Using ${weave_pod}..."
   [[ -n ${weave_pod} ]]
+  node_regex=$(kubectl get nodes --no-headers -o=custom-columns=NAME:.metadata.name | sed -E "s/ip-(.*)\.ec2.*/\1$/" | paste -d '|' -s -)
   kubectl exec -i -c weave -n kube-system ${weave_pod} -- /bin/sh -c \
     'curl -s http://127.0.0.1:6784/status/ipam | \
     grep unreachable\\!$ | \
     sed -E "s/.*\(ip-([0-9-]+).*/127.0.0.1:6784\/peer\/\1/" | \
     sort | \
-    uniq | \
+    uniq | grep -Ev "('"${node_regex}"')" | \
     xargs -n 1 curl -sX DELETE'
   echo "Done."
   touch /lastloop
